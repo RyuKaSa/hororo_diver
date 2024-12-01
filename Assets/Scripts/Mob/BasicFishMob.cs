8560aeab96b7bb79205fe11914d831aa60016b0f
Vlt09 <valentin.barrau@free.fr>
2024-12-01 15:22:05 +0100

    feat: adding Attack Sequence manage

--- Assets/Scripts/Mob/BasicFishMob.cs
+++ Assets/Scripts/Mob/BasicFishMob.cs
@@ -7,6 +7,58 @@ using UnityEngine;
 /// </summary>
 public sealed class BasicFishMob : MonoBehaviour
 {
+
+    /// <summary>
+    /// Enum which describe all stages during an attack sequence
+    /// </summary>
+    private enum ATTACK_STAGE
+    {
+        BEGIN_STG, // Stage when calculate data for attack
+        BACKWARD_STG, // Stage where the mob backward before charge player
+        ATK_STG, // Stage where the mob charges player
+        RETURN_STG, // Stage where the mob returns to his start point
+        NOATK
+    };
+
+    /// <summary>
+    /// Class containing all the data necessary during a mob attack sequence.
+    /// </summary>
+    private sealed class AttackSequenceData
+    {
+        private Vector3 startPoint;
+        private float backwardDistance; // Distance between startPoint and the point behind mob
+
+        private float speedCharge; // Speed when the mob charge the player after BACKWARD_STG
+
+        public void SetStartPoint(Vector3 startPoint)
+        {
+            this.startPoint = startPoint;
+        }
+
+        public void SetBackWardDistance(float backwardDistance)
+        {
+            if (this.backwardDistance < 0)
+            {
+                Debug.Log("ERROR : BackWardDistance is negative");
+                return;
+            }
+            this.backwardDistance = backwardDistance;
+        }
+
+        public void SetSpeedCharge(float speedCharge)
+        {
+            if (this.speedCharge <= 0)
+            {
+                Debug.Log("ERROR : speedCharge is negative or equal to 0");
+                return;
+            }
+
+            this.speedCharge = speedCharge;
+        }
+
+
+    }
+
     private Mob mob;
     public UnityEngine.AI.NavMeshAgent agent;
 
@@ -20,11 +72,11 @@ public sealed class BasicFishMob : MonoBehaviour
 
     private float moveAreaRange = 20f;
 
+    private float attackRange = 2f;
 
-    private void HuntingPlayer()
-    {
+    private ATTACK_STAGE atk_stage = ATTACK_STAGE.NOATK;
 
-    }
+    private AttackSequenceData attackSequenceData = new AttackSequenceData();
 
     public void Start()
     {
@@ -34,19 +86,44 @@ public sealed class BasicFishMob : MonoBehaviour
     }
 
     /// <summary>
-    /// This method call different function which manage behavior according
+    /// This method calls different function which manage behavior according
     /// to Mob's state.
     /// </summary>
-    private void BehaviorProcessBasedOnState(Mob.State state)
+    private void BehaviorProcessBasedOnState(GameObject player, Mob.State state)
     {
         if (state == Mob.State.HUNTING)
         {
             Debug.Log("Follow Player");
             mob.SetMobAgentDestination(player.transform.position);
+            AttackSequenceProcess(player);
         }
         else
         {
             mob.PassiveMobMovement();
+        }
+    }
+
+    /// <summary>
+    /// Function managing an attack sequence when the mob is at a certain distance from the player.
+    /// An attack sequence is broken down into 4 phases. The first consists of initializing the data relating to the attack
+    /// (the position of the mob and the distance between mob and the backward point). The second consists of the mob 
+    /// going to a point behind it before rushing towards the player to attack. The third is when the mob rush to the player and attacks.
+    /// The last phase consists of go back to start point.
+    /// </summary>
+    /// <param name="player"></param>
+    private void AttackSequenceProcess(GameObject player)
+    {
+        float distMobPlayer = Vector3.Distance(transform.position, player.transform.position);
+        // Trigger firt stage
+        if (atk_stage == ATTACK_STAGE.NOATK && distMobPlayer <= attackRange)
+        {
+            Debug.Log("CHANGE TO BACKWARD STAGE");
+
+            atk_stage = ATTACK_STAGE.BACKWARD_STG;
+            float backwardDistance = distMobPlayer * 0.05f;
+            attackSequenceData.SetBackWardDistance(backwardDistance);
+            attackSequenceData.SetStartPoint(transform.position);
+            mob.SetMobAgentDestination(new Vector3(transform.position.x - backwardDistance, transform.position.y, transform.position.z));
         }
     }
 
@@ -60,7 +137,7 @@ public sealed class BasicFishMob : MonoBehaviour
         else
         {
             var state = mob.HandleStateBasedOnSight(player, transform.position); // Update mob current state
-            BehaviorProcessBasedOnState(state); // Determines which behavior algo choose according to mob's state
+            BehaviorProcessBasedOnState(player, state); // Determines which behavior algo choose according to mob's state
         }
     }
 
