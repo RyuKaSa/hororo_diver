6862c19f04cea9779e8c506a92211aa50c461439
Lawayn <mailis.bonhommr@gmail.com>
2024-12-04 19:35:36 +0100

    Base UI

---
+++ Assets/Scripts/UI/InventoryData.cs
@@ -0,0 +1,25 @@
+﻿[System.Serializable]
+public class InventoryData
+{
+    public InventoryData(int _slotCount)
+    {
+        items = new Item[_slotCount];
+    }
+
+    public Item[] items { private set; get; }
+
+    public bool SlotAvailable(Item _itemToAdd)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Item AddItem(Item _itemToAdd)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Item Pick(int _slotID)
+    {
+        throw new System.NotImplementedException();
+    }
+}
