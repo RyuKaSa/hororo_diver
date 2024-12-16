1aa7b4a4c317172d5840e8d0cc21aba23da38572
Vlt09 <valentin.barrau@free.fr>
2024-12-16 23:16:52 +0100

    fix: missing file previous commit

--- Assets/Scripts/Player/Weapons/LongRangeWeapon.cs
+++ Assets/Scripts/Player/Weapons/LongRangeWeapon.cs
@@ -25,7 +25,20 @@ public sealed class LongRangeWeapon : MonoBehaviour, IWeapons
     {
         Debug.Log("Player attack with " + transform.name);
 
-        var projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        var player = GameObject.Find("Player");
+        var direction = player.transform.position - transform.position;
+        direction.z = 0;
+        float angle = Mathf.Atan2(direction.normalized.y, direction.normalized.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+
+        // Calculate Linear equation with mob position and player position
+        float gradient = (player.transform.position.y - transform.position.y) / (player.transform.position.x - transform.position.x);
+        float offset = transform.position.y - (gradient * transform.position.x);
+
+
+        var projectileGameObject = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        var projectile = projectileGameObject.GetComponent<Projectile>();
+        projectile.Initialize(0.5f, gradient, offset, 1f);
 
         // Detects ennemies in range of attack
         // var hitEnnemiesArray = Physics2D.OverlapCircleAll(transform.position, attackRange, enemyLayer);
