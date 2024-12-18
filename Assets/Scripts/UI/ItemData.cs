a21f050d746a3e04525a3c631c4b060f7d062977
Lawayn <mailis.bonhommr@gmail.com>
2024-12-18 17:35:17 +0100

    Last change

--- Assets/Scripts/UI/ItemData.cs
+++ Assets/Scripts/UI/ItemData.cs
@@ -3,11 +3,39 @@
 [CreateAssetMenu(menuName ="Items/Item Data")]
 public class ItemData : ScriptableObject
 {
-    [SerializeField] public string itemName;
+    /*[SerializeField] public string itemName;
 
     [SerializeField] public int stackMaxCount = 1;
 
-    [SerializeField] public Sprite icon;
+    [SerializeField] public Sprite icon;*/
+
+    public enum ItemType
+    {
+        Ore_Armor,
+        Ore_Ammunition,
+        Weapon,
+        Tool,
+        Artifact
+    }
+
+    [Header("Basic Item Properties")]
+    public string itemName;
+    public ItemType itemType;
+    public Sprite icon;
+    public int stackMaxCount = 1;
+
+    [Header("Ore Specific")]
+    public int miningLevel; // Which levels this ore can be found in
+    public bool isRare;
+
+    [Header("Weapon Specific")]
+    public float baseDamage;
+    public bool isCraftable;
+    public string[] requiredOres; // Ores needed to craft this weapon
+
+    [Header("Artifact Specific")]
+    public bool isUnique;
+    public bool isMandatory; // Unmissable artifact
 }
 
 public interface IConsumable
---
+++ Assets/Scripts/UI/ItemDatabase.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class ItemDatabase : MonoBehaviour
+{
+    public static ItemDatabase Instance { get; private set; }
+
+    [Header("Ores")]
+    public OreData manganeseOre;
+    public OreData copperOre;
+    public OreData cobaltOre;
+    public OreData ironOre;
+
+    [Header("Weapons")]
+    public WeaponData harpoonHandgun;
+    public WeaponData whalingHarpoonCannon;
+    public WeaponData knife;
+    public WeaponData pickaxe;
+
+    [Header("Artifacts")]
+    public ItemData[] artifacts;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public ItemData GetItemByName(string itemName)
+    {
+        // Search through all defined items
+        ItemData[] allItems = new ItemData[]
+        {
+        manganeseOre, copperOre, cobaltOre, ironOre,
+        harpoonHandgun, whalingHarpoonCannon, knife, pickaxe
+        };
+
+        // Concatenate artifacts to the array
+        allItems = allItems.Concat(artifacts).ToArray();
+
+        return allItems.FirstOrDefault(item => item != null && item.itemName == itemName);
+    }
+}
