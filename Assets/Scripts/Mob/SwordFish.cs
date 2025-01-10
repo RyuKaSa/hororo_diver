33a3728faddb82f483c9ddd535ff6fca6bedcc90
Vlt09 <valentin.barrau@free.fr>
2025-01-10 18:50:41 +0100

    feat: add state machine and attack logic

--- Assets/Scripts/Mob/SwordFish.cs
+++ Assets/Scripts/Mob/SwordFish.cs
@@ -6,7 +6,7 @@ using System;
 
 public sealed class SwordFish : MonoBehaviour
 {
-    enum State
+    enum SwordFishState
     {
         PASSIVE,
         HUNTING,
@@ -37,22 +37,151 @@ public sealed class SwordFish : MonoBehaviour
     [SerializeField]
     private float attackRange = 2f;
 
+    [SerializeField]
+    private float chargeTime = 2f;
+
+    [SerializeField]
+    private float chargeSpeedCoeff = 5f;
+
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private float timeBetween2Charge = 2f;
+
+    private float timeToReachPlayer;
+
+    private Vector3 lastPlayerPos;
+
     private Mob mob;
 
-    private State state = State.PASSIVE;
+    private SwordFishState state = SwordFishState.PASSIVE;
 
-    private StateMachine<State> stateMachine;
+    private StateMachine<SwordFishState> stateMachine = new StateMachine<SwordFishState>();
+
 
     void Start()
     {
         mob = new Mob(agent, health, speed, visionRange, moveAreaRange, transform.position);
         mob.Start();
 
+        stateMachine.AddState(SwordFishState.PASSIVE, new State<SwordFishState>(
+            onLogic: state => mob.PassiveMobMovement()
+        ));
+
+        stateMachine.AddState(SwordFishState.CHARGE, new State<SwordFishState>(
+            onLogic: state => ChargeAttackProcess(state.timer.Elapsed),
+            canExit: state => state.timer.Elapsed > chargeTime,
+            needsExitTime: true
+        ));
+
+        stateMachine.AddState(SwordFishState.RUSH, new State<SwordFishState>(
+            onLogic: state => RushTowardsPlayer(),
+            canExit: state => Vector3.Distance(transform.position, lastPlayerPos) <= 1f && state.timer.Elapsed >= timeBetween2Charge,
+            needsExitTime: true
+        ));
+
+        stateMachine.AddTransition(new Transition<SwordFishState>(
+            SwordFishState.PASSIVE,
+            SwordFishState.CHARGE,
+            transition => mob.HandleStateBasedOnSight(player, transform.position) == Mob.State.HUNTING
+        ));
+
+        stateMachine.AddTransition(new Transition<SwordFishState>(
+            SwordFishState.CHARGE,
+            SwordFishState.PASSIVE,
+            transition => mob.HandleStateBasedOnSight(player, transform.position) == Mob.State.PASSIVE
+        ));
+
+        stateMachine.AddTransition(new Transition<SwordFishState>(
+            SwordFishState.CHARGE,
+            SwordFishState.RUSH,
+            transition => true
+        ));
+
+        stateMachine.AddTransition(new Transition<SwordFishState>(
+            SwordFishState.RUSH,
+            SwordFishState.CHARGE,
+            transition => true
+        ));
+
+
+        stateMachine.Init();
+
+    }
+
+    /// <summary>
+    /// Handles the logic when the sword fish charge his attck.
+    /// This method calculates the necessary speed, rotation, and visual effects
+    /// for the SwordFish before to charge towards the player
+    /// </summary>
+    /// <param name="elapsedTime">The elapsed time since the start of the charge state</param>
+    private void ChargeAttackProcess(float elapsedTime)
+    {
+        // Disable the NavMeshAgent to stop random movement
+        if (agent.enabled == true)
+        {
+            agent.enabled = false;
+        }
+
+        // saves the player's position up to a certain time
+        if (elapsedTime <= chargeTime * 0.98)
+        {
+            lastPlayerPos = player.transform.position;
+
+            float chargeSpeed = chargeSpeedCoeff * speed;
+            float distanceToPlayer = Vector3.Distance(transform.position, lastPlayerPos);
+
+            timeToReachPlayer = distanceToPlayer / chargeSpeed;
+        }
+
+        // Face to player during his charge
+        Vector3 directionToPlayer = player.transform.position - transform.position;
+        directionToPlayer.z = 0; // Ensure we remain on the 2D plane
+
+        float angle = Mathf.Atan2(directionToPlayer.y, directionToPlayer.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+        ShakeEffect();
+    }
+
+
+    /// <summary>
+    /// Moves the SwordFish towards the player's last known position at a certain charge speed.
+    /// The SwordFish will continue moving towards the player until it is close enough.
+    /// </summary>
+    /// <param name="chargeSpeedCoeff">A coefficient that scales the base speed of the SwordFish's charge.</param>
+    /// <param name="speed">The base speed of the SwordFish (before applying the charge speed coefficient).</param>
+    private void RushTowardsPlayer()
+    {
+        // Calculate the direction from the SwordFish to the player's last known position
+        Vector3 directionToPlayer = lastPlayerPos - transform.position;
+        directionToPlayer.z = 0; // Ignore Z-axis
+        directionToPlayer.Normalize();
+
+        float chargeSpeed = chargeSpeedCoeff * speed;
+        float distanceToPlayer = Vector3.Distance(transform.position, lastPlayerPos);
+
+        // Move the SwordFish towards the player if it is farther than 1 unit
+        if (distanceToPlayer > 1f)
+        {
+            transform.position += directionToPlayer * chargeSpeed * Time.deltaTime;
+        }
+    }
+
+    private void ShakeEffect()
+    {
+        var shakeX = Mathf.Sin(Time.time * 10f) * 0.005;
+        var shakeY = Mathf.Cos(Time.time * 10f) * 0.005;
+
+        transform.position += new Vector3((float)shakeX, (float)shakeY, 0f);
     }
 
     void Update()
     {
-        var player = GameObject.Find("Player");
+        stateMachine.OnLogic();
+        /*var player = GameObject.Find("Player");
         if (player == null)
         {
             Debug.Log("PLAYER NULL");
@@ -86,7 +215,7 @@ public sealed class SwordFish : MonoBehaviour
                 mob.PassiveMobMovement();
             }
 
-        }
+        }*/
 
     }
 }
